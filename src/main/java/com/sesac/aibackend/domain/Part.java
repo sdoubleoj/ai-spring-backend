f1ac0a10660c60ca4d0a12db8930ea72254461c8
sdoubleoj <gjtnwjd329@gmail.com>
2026-06-04 17:22:38 +0900

    [CREATE] Part domain

---
+++ src/main/java/com/sesac/aibackend/domain/Part.java
@@ -0,0 +1,20 @@
+package com.sesac.aibackend.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Part {
+    
+    private String partNumber;
+    private String name;
+    private String category;
+    private String supplier;
+    private String unit;
+    private int stock;
+    private int safetystock;
+    private String warehouseLocation;
+}
---
+++ src/main/java/com/sesac/aibackend/service/CarService.java
@@ -0,0 +1,10 @@
+package com.sesac.aibackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CarService {
+
+}
