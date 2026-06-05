c74fb1af87f4ba502815f45c2fd2e7482c63f4a3
sdoubleoj <gjtnwjd329@gmail.com>
2026-06-05 17:18:05 +0900

    CREATE Department domain

---
+++ src/main/java/com/sesac/aibackend/domain/Department.java
@@ -0,0 +1,39 @@
+package com.sesac.aibackend.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            unique = true,
+            nullable = false,
+            length = 100
+    )
+    private String name;
+
+    // 부서 내 직원 수
+    @Column(
+            name = "employee_count",
+            nullable = false
+    )
+    private int employeeCount = 0;
+
+    @Enumerated(EnumType.STRING)
+    @Column(
+            nullable = false,
+            length = 20
+    )
+    private Role role;
+
+}
